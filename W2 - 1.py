9afe02d8ddea43d0002158c1a4519c3537e59d62
Sara Uyen Tran <utra0001@student.monash.edu>
2019-11-09 22:15:41 +1100

    Add comments

--- W2 - 1.py
+++ W2 - 1.py
@@ -1,15 +1,21 @@
-# cook your dish here
+# I'm gonna be fairly nit-picking just to warn ya
+# Overall the logic is fine
+# But this would require you to manually input every number and manually record the number of operations
+# Why not automate that?
 
+# todo: make the variable name descriptive please, not too big of a problem for a small program but boi it can be a mess
+# todo: I suggest changing this to sth like "number_of_operations"
 o = 0
 
-n = int(input("Enter a number: "))  
-  
-if n > 1:  
-   for i in range(2,n):
-       o+=1
-       if (n % i) == 0:  
-           print(n,"is not a prime number")  
-           break  
-   else:  
-       print(n,"is a prime number")  
-       print("The number of operation(s) is: ",o)
+# todo: better variable name
+n = int(input("Enter a number: "))
+
+if n > 1:
+    for i in range(2, n):
+        o += 1
+        if (n % i) == 0:
+            print(n, "is not a prime number")
+            break
+    else:
+        print(n, "is a prime number")
+        print("The number of operation(s) is: ", o)
